4bb5c9af055e4bc6bec2eaf63a0b50bffc1de999
David Bai <david.bai@warriorlife.net>
2017-12-13 14:04:01 -0800

    Minor bug fixes
    Fixed classsification to element, and added img
    Changed memory buttons to parentheses

--- calculator.js
+++ calculator.js
@@ -6,17 +6,17 @@ function d(val) {
 	}
 	
 	//type numbers and operators
-	function v(val) {
-		document.getElementById("d").value +=val;
-		}
+function v(val) {
+	document.getElementById("d").value +=val;
+	}
 		
 		//evaluate the equation
-		function e() {
-			try{
-				d(eval(document.getElementById("d").value));
-			}
-			catch(err) {
-				d("error");
-				}
-				
-			}
+function e() {
+	try{
+		d(eval(document.getElementById("d").value));
+	}
+	catch(err) {
+		d("error");
+		}
+		
+	}
